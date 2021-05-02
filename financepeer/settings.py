db673cfcd3a57d56c38a3969ea38ed307295f48b
B-UDAY-CHANDRA-001 <56556648+B-UDAY-CHANDRA-001@users.noreply.github.com>
2021-05-02 19:25:17 +0530

    Update settings.py

--- financepeer/settings.py
+++ financepeer/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uuly=0o8in2$*ftgds065^@xju7t@j+81*k1u^+(%#5%-v3!%x'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
